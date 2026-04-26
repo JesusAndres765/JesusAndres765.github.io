02f4972a84ec683e6c051fd231629a12c54dd0fb
JesusAndres765 <jesusaw065@gmail.com>
2026-04-26 12:10:07 -0600

    AJ

--- src/app/models/header/header.model.ts
+++ src/app/models/header/header.model.ts
@@ -2,7 +2,7 @@ export class Header {
     id?: string;
     name?: string = 'name';
     goalLife?: string = 'goal';
-    photoUrl?: string = 'photo';
+    photoUrl?: string = '';
     email?: string = 'email@domain.com';
     phoneNumber?: string = '999-999-9999';
     location?: string = 'city, country';
